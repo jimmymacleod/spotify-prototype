fbbcaec463c2bbe253387fd79132392297c64adb
James MacLeod <james@macdoch.com>
2018-05-15 09:33:22 +1000

    added playlists

--- src/App.js
+++ src/App.js
@@ -6,15 +6,44 @@ let defaultStyle = {
   color: "LimeGreen"
 };
 
+let fakeServerData = {
+  user: {
+    name: "James",
+    playlists: [
+      {
+        name: "Favorites",
+        songs: ["Lose Yourself to Dance", "Levels", "Not Giving In"]
+      },
+      {
+        name: "Discover",
+        songs: ["Cry Me A River", "Hans Solo Dies", "Third Orchestra"]
+      },
+      {
+        name: "Hot Ones",
+        songs: ["In Da Club", "Matchmacker", "Chronic"]
+      },
+      {
+        name: "Sick Boy",
+        songs: ["Rolling Stone", "La Women", "Fix You"]
+      }
+    ]
+  }
+};
+
 class Aggregator extends Component {
   render() {
     return (
       <div style={{ width: "40%", display: "inline-block" }}>
-        <h2 style={{ ...defaultStyle }}> Number Text </h2>
+        <h2 style={{ ...defaultStyle }}>
+          {" "}
+          {this.props.playlists && this.props.playlists.length} Text
+        </h2>
       </div>
     );
   }
 }
+
+// If there is an object there, then find the length of that object
 
 class Filter extends Component {
   render() {
@@ -44,13 +73,29 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { serverData: {} };
+  }
+  componentDidMount() {
+    this.setState({ serverData: fakeServerData });
+  }
   render() {
     return (
       <div className="App">
-        <h1 style={{ ...defaultStyle }}>Title</h1>
-        <Aggregator />
+        <h1 style={{ ...defaultStyle }}>
+          {this.state.serverData.user && this.state.serverData.user.name}s
+          Playlists
+        </h1>
+        <Aggregator
+          playlists={
+            this.state.serverData.user && this.state.serverData.user.playlists
+          }
+        />
         <Aggregator />
         <Filter />
+        <Playlist />
+        <Playlist />
         <Playlist />
         <Playlist />
       </div>
